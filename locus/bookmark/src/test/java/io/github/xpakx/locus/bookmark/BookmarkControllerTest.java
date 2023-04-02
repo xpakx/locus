740cde9b08cea1e700c756230f01b2605d471c96
xpakx <55586470+xpakx@users.noreply.github.com>
2023-04-02 16:25:59 +0200

    test(bookmark): test saving bookmark

---
+++ locus/bookmark/src/test/java/io/github/xpakx/locus/bookmark/BookmarkControllerTest.java
@@ -0,0 +1,124 @@
+package io.github.xpakx.locus.bookmark;
+
+import io.github.xpakx.locus.bookmark.dto.BookmarkRequest;
+import io.github.xpakx.locus.downloader.UrlReaderService;
+import io.github.xpakx.locus.elasticsearch.ElasticSearchAspect;
+import io.github.xpakx.locus.security.JwtUtils;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.http.HttpStatus.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BookmarkControllerTest {
+    @LocalServerPort
+    private int port;
+    private String baseUrl;
+
+    @Autowired
+    JwtUtils jwt;
+    @Autowired
+    BookmarkRepository courseRepository;
+
+    @MockBean
+    UrlReaderService urlReaderService;
+
+    @MockBean
+    ElasticSearchAspect aspect;
+
+    @BeforeEach
+    void setUp() {
+        baseUrl = "http://localhost".concat(":").concat(port + "/api/v1/bookmarks");
+    }
+
+    @AfterEach
+    void tearDown() {
+        courseRepository.deleteAll();
+    }
+    private String tokenFor(String username) {
+        return tokenFor(username, new ArrayList<>());
+    }
+
+    private String tokenFor(String username, List<GrantedAuthority> authorities) {
+        return jwt.generateToken(new User(username, "", authorities));
+    }
+
+    @Test
+    void shouldRespondWith401ToBookmarkPageIfNotAuthenticated() {
+        when()
+                .post(baseUrl)
+        .then()
+                .log().body()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void shouldRespondWith401ToBookmarkPageIfTokenIsWrong() {
+        given()
+                .auth()
+                .oauth2("329432853295")
+                .contentType(ContentType.JSON)
+                .body(getBookmarkRequest("http://example.com"))
+        .when()
+                .post(baseUrl)
+        .then()
+                .log().body()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    private BookmarkRequest getBookmarkRequest(String url) {
+        return new BookmarkRequest(url);
+    }
+    @Test
+    void shouldBookmarkPage() throws IOException {
+        doReturn("").when(urlReaderService).read(any(URL.class));
+        given()
+                .auth()
+                .oauth2(tokenFor("user1"))
+                .contentType(ContentType.JSON)
+                .body(getBookmarkRequest("http://example.com"))
+        .when()
+                .post(baseUrl)
+        .then()
+                .log().body()
+                .statusCode(OK.value())
+                .body("url", equalTo("http://example.com"));
+    }
+    @Test
+    void shouldBookmarkPageWithCorrectContent() throws IOException {
+        doReturn("webpage content").when(urlReaderService).read(any(URL.class));
+        given()
+                .auth()
+                .oauth2(tokenFor("user1"))
+                .contentType(ContentType.JSON)
+                .body(getBookmarkRequest("http://example.com"))
+        .when()
+                .post(baseUrl)
+        .then()
+                .log().body()
+                .statusCode(OK.value())
+                .body("url", equalTo("http://example.com"))
+                .body("content", equalTo("webpage content"));
+    }
+}
