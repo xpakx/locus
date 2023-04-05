85c43d83fcb272c599931275858789039f3c5ae7
xpakx <55586470+xpakx@users.noreply.github.com>
2023-04-05 15:26:12 +0200

    chore(bookmark): add authorization to elasticsearch

--- locus/bookmark/src/main/java/io/github/xpakx/locus/settings/ElasticSearchConfig.java
+++ locus/bookmark/src/main/java/io/github/xpakx/locus/settings/ElasticSearchConfig.java
@@ -5,6 +5,10 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +16,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ElasticSearchConfig {
     @Bean
+    public CredentialsProvider getCredentialsProvider() {
+        final CredentialsProvider credentialsProvider =
+                new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials("elastic", "password"));
+        return credentialsProvider;
+    }
+
+    @Bean
     public RestClient getRestClient() {
-        return RestClient.builder(new HttpHost("localhost", 9200)).build();
+        return RestClient
+                .builder(new HttpHost("localhost", 9200))
+                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
+                        .setDefaultCredentialsProvider(getCredentialsProvider()))
+                .build();
     }
 
     @Bean
