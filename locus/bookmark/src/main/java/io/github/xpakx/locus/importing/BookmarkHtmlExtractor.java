a4b1a7d923acc973f49f6527f57ec15fc9d60444
xpakx <55586470+xpakx@users.noreply.github.com>
2023-04-09 17:32:31 +0200

    feat(bookmark): add service fro extracting bookmarks from html

---
+++ locus/bookmark/src/main/java/io/github/xpakx/locus/importing/BookmarkHtmlExtractor.java
@@ -0,0 +1,27 @@
+package io.github.xpakx.locus.importing;
+
+import io.github.xpakx.locus.bookmark.Bookmark;
+import io.github.xpakx.locus.bookmark.dto.BookmarkRequest;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookmarkHtmlExtractor {
+
+    public List<BookmarkRequest> extractBookmarks(String html) {
+        return Jsoup
+                .parse(html)
+                .getElementsByTag("a")
+                .stream()
+                .map(this::toBookmark)
+                .toList();
+    }
+
+    private BookmarkRequest toBookmark(Element link) {
+        return new BookmarkRequest(link.attr("href"));
+    }
+}
