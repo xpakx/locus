16d070864f66c018bee112fed8f7eaa3f5304a58
xpakx <55586470+xpakx@users.noreply.github.com>
2023-04-21 15:47:08 +0200

    test(bookmark): test untagging bookmark

--- locus/bookmark/src/test/java/io/github/xpakx/locus/bookmark/TagControllerTest.java
+++ locus/bookmark/src/test/java/io/github/xpakx/locus/bookmark/TagControllerTest.java
@@ -19,7 +19,9 @@ import org.springframework.security.core.userdetails.User;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
@@ -163,10 +165,93 @@ class TagControllerTest {
         assertThat(tagRepository.findAll(), hasSize(1));
     }
 
-    private void addTag(String name, String owner) {
+    private Long addTag(String name, String owner) {
         Tag tag = new Tag();
         tag.setName(name);
         tag.setOwner(owner);
-        tagRepository.save(tag);
+        return tagRepository.save(tag).getId();
+    }
+
+    @Test
+    void shouldRespondWith401ToUntagBookmarkIfNotAuthenticated() {
+        when()
+                .delete(baseUrl + "/{bookmarkId}/tags/{tag}", 1L, "tag")
+        .then()
+                .log().body()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void shouldRespondWith401ToUntagBookmarkIfTokenIsWrong() {
+        given()
+                .auth()
+                .oauth2("329432853295")
+        .when()
+                .delete(baseUrl + "/{bookmarkId}/tags/{tag}", 1L, "tag")
+        .then()
+                .log().body()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void shouldRespondWith404ToUntagBookmarkIfPageNotBookmarked() throws IOException {
+        given()
+                .auth()
+                .oauth2(tokenFor("user1"))
+        .when()
+                .delete(baseUrl + "/{bookmarkId}/tags/{tag}", 1L, "tag")
+        .then()
+                .log().body()
+                .statusCode(NOT_FOUND.value());
+    }
+
+    @Test
+    void shouldRespondWith404ToUntagBookmarkIfBookmarkDoesNotBelongToUser() throws IOException {
+        Long id = addBookmark("http://example.com", "user2");
+        given()
+                .auth()
+                .oauth2(tokenFor("user1"))
+        .when()
+                .delete(baseUrl + "/{bookmarkId}/tags/{tag}", id, "tag")
+        .then()
+                .log().body()
+                .statusCode(NOT_FOUND.value());
+    }
+
+    @Test
+    void shouldRespondWith404ToUntagBookmarkIfTagDoesNotExist() throws IOException {
+        Long id = addBookmark("http://example.com", "user1");
+        given()
+                .auth()
+                .oauth2(tokenFor("user1"))
+        .when()
+                .delete(baseUrl + "/{bookmarkId}/tags/{tag}", id, "tag")
+        .then()
+                .log().body()
+                .statusCode(NOT_FOUND.value());
+    }
+
+    @Test
+    void shouldUntagBookmark() throws IOException {
+        Long id = addBookmark("http://example.com", "user1");
+        Long tagId = addTag("tag", "user1");
+        tagBookmark(id, tagId);
+        given()
+                .auth()
+                .oauth2(tokenFor("user1"))
+       .when()
+                .delete(baseUrl + "/{bookmarkId}/tags/{tag}", id, "tag")
+       .then()
+                .log().body()
+                .statusCode(OK.value());
+    }
+
+    private void tagBookmark(Long bookmarkId, Long tagId) {
+        Set<Tag> tags = new HashSet<>();
+        tags.add(tagRepository.findById(tagId).get());
+        Bookmark bookmark = bookmarkRepository.findById(bookmarkId)
+                .get();
+        bookmark.setTags(tags);
+        bookmarkRepository.save(bookmark);
     }
 }
