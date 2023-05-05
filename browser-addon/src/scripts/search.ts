c855683fb7604ac3b3117d18a8c7f3bbe63c6076
xpakx <55586470+xpakx@users.noreply.github.com>
2023-05-05 13:19:22 +0200

    feat(addon): fetch all bookmarks from api

--- browser-addon/src/scripts/search.ts
+++ browser-addon/src/scripts/search.ts
@@ -2,44 +2,70 @@ import { BookmarkSummary } from "./dto/bookmark-summary"
 
 var bookmarkContainer: HTMLElement | null = null;
 var bookmarkTemplate: string = "";
+const apiUri = "http://localhost:8000/api/v1";
+var token = null;
+
+chrome.storage.local.get('token', function (result) {
+    if (result.token) {
+        token = result.token;
+    }
+});
 
 const testBookmarks: BookmarkSummary[] = [
-    {id: 1, name: "Bookmark 1", url: "https://example.com/1", date: ""},
-    {id: 2, name: "Bookmark 2", url: "https://example.com/2", date: ""},
-    {id: 3, name: "Bookmark 3", url: "https://example.com/3", date: ""},
-    {id: 4, name: "Bookmark 4", url: "https://example.com/4", date: ""},
+    { id: 1, name: "Bookmark 1", url: "https://example.com/1", date: "" },
+    { id: 2, name: "Bookmark 2", url: "https://example.com/2", date: "" },
+    { id: 3, name: "Bookmark 3", url: "https://example.com/3", date: "" },
+    { id: 4, name: "Bookmark 4", url: "https://example.com/4", date: "" },
 ]
 
 document.addEventListener('DOMContentLoaded', function () {
     bookmarkContainer = document.querySelector(".bookmarks");
     fetch(chrome.runtime.getURL("pages/bookmark.html"))
-      .then(response => response.text())
-      .then(html => {
-        bookmarkTemplate = html;
-        showTestBookmarks(testBookmarks);
-    });
+        .then(response => response.text())
+        .then(html => {
+            bookmarkTemplate = html;
+            showBookmarks(testBookmarks);
+            getAllBookmarks();
+        });
 });
-  
-function showTestBookmarks(bookmarks: BookmarkSummary[]) {
-    if(!bookmarkContainer) {
+
+function showBookmarks(bookmarks: BookmarkSummary[]) {
+    if (!bookmarkContainer) {
         return;
     }
-    for(let bookmark of bookmarks) {
+    for (let bookmark of bookmarks) {
         var bookmarkElem = document.createElement("div");
         bookmarkElem.classList.add("bookmark");
         bookmarkElem.innerHTML = bookmarkTemplate;
         var bookmarkName = bookmarkElem.querySelector(".bookmark-name");
-        if(bookmarkName) {
+        if (bookmarkName) {
             bookmarkName.innerHTML = bookmark.name;
         }
         var bookmarkDate = bookmarkElem.querySelector(".date");
-        if(bookmarkDate) {
+        if (bookmarkDate) {
             bookmarkDate.innerHTML = bookmark.date;
         }
-        var bookmarkLink = <HTMLAnchorElement> bookmarkElem.querySelector(".bookmark-link");
-        if(bookmarkLink) {
+        var bookmarkLink = <HTMLAnchorElement>bookmarkElem.querySelector(".bookmark-link");
+        if (bookmarkLink) {
             bookmarkLink.href = bookmark.url;
         }
         bookmarkContainer.insertBefore(bookmarkElem, bookmarkContainer.firstChild);
     }
+}
+
+function getAllBookmarks() {
+    fetch(`${apiUri}/bookmarks/all`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+        .then(response => response.json())
+        .then((data: BookmarkSummary[]) => {
+            showBookmarks(data);
+        })
+        .catch(error => {
+            console.error('An error occurred:', error);
+        });
 }
