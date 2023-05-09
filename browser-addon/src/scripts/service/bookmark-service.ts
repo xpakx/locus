bee77fa2f3d353c74b0dc66554a8c434f977e016
xpakx <55586470+xpakx@users.noreply.github.com>
2023-05-09 19:57:20 +0200

    refactor(addon): extract generation of headers to new method

--- browser-addon/src/scripts/service/bookmark-service.ts
+++ browser-addon/src/scripts/service/bookmark-service.ts
@@ -9,22 +9,23 @@ export class BookmarkService {
     async fetchAllBookmarks(token?: string): Promise<BookmarkSummary[]> {
         const response = await fetch(`${this.apiUri}/bookmarks/all`, {
             method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            }
+            headers: this.getHeaders(token)
         });
         return await response.json();
+    }
+
+    private getHeaders(token: string | undefined): HeadersInit | undefined {
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        };
     }
 
     async searchBookmarksByString(searchString: string, token?: string) {
         const params = new URLSearchParams({ searchString: searchString });
         const response = await fetch(`${this.apiUri}/bookmarks?` + params, {
             method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            }
+            headers: this.getHeaders(token)
         });
         return await response.json();
     }
@@ -35,10 +36,7 @@ export class BookmarkService {
             `${this.apiUri}/bookmarks/check?` + params,
             {
                 method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + token
-                }
+                headers: this.getHeaders(token)
             }
         )
         return await response.json();
@@ -48,10 +46,7 @@ export class BookmarkService {
     async addBookmark(url: string, token?: string) {
         const response = await fetch(`${this.apiUri}/bookmarks`, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            },
+            headers: this.getHeaders(token),
             body: JSON.stringify({
                 url: url
             })
@@ -62,10 +57,7 @@ export class BookmarkService {
     async deleteBookmark(id: number, token?: string) {
         const response = await fetch(`${this.apiUri}/bookmarks/${id}`, {
             method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            }
+            headers: this.getHeaders(token)
         })
         return await response.json();
     }
